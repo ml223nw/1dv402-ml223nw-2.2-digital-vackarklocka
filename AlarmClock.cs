58d6a5636b3a1070cd36949eeee9358d45ede4cd
Mathias Lundberg <tias86@gmail.com>
2014-10-09 16:13:34 +0200

    Added fields

---
+++ AlarmClock.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_2_digital_vackarklocka
+{
+    public class AlarmClock
+    {
+        private int _alarmHour;
+        private int _alarmMinute;
+        private int _hour;
+        private int _minute;
+    }
+
+    public AlarmClock()
+    {
+    }
+
+
+}
