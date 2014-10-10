7c3ee5497783cbc9b2b82362e83ea73495527109
Mathias Lundberg <tias86@gmail.com>
2014-10-10 17:18:18 +0200

    Added  constructors and TickTock method

--- AlarmClock.cs
+++ AlarmClock.cs
@@ -6,78 +6,118 @@ using System.Threading.Tasks;
 
 namespace _2_2_digital_vackarklocka
 {
-    public class AlarmClock
-    {
-    // Fält.
-        private int _alarmHour;
-        private int _alarmMinute;
-        private int _hour;
-        private int _minute;
-    }
-
-    // Inkaplsade fält.
-    public int AlarmHour
-    {
-        get { return _alarmHour; }
-        set
+        public class AlarmClock
         {
-                if (value < 0 || value > 23)
-            {
-                throw new ArgumentException();
-            }
-            else {_alarmHour = value;}
+        // Fält.
+            private int _alarmHour;
+            private int _alarmMinute;
+            private int _hour;
+            private int _minute;
         }
 
-    }
-
-
-    public int AlarmMinute
-    {
-        get { return _alarmMinute; }
-        set
+        // Klockans egenskaper valideras.
+        public int AlarmHour
         {
-                if (value < 0 || value > 59)
+            get { return _alarmHour; }
+            set
             {
-                throw new ArgumentException();
+                    if (value < 0 || value > 23)
+                {
+                    throw new ArgumentException();
+                }
+                else {_alarmHour = value;}
             }
-            else {_alarmMinute = value;}
+
         }
 
-    }
 
-    public int Hour
-    {
-        get { return _hour; }
-        set
+        public int AlarmMinute
         {
-                if (value < 0 || value > 23)
+            get { return _alarmMinute; }
+            set
             {
-                throw new ArgumentException();
+                    if (value < 0 || value > 59)
+                {
+                    throw new ArgumentException();
+                }
+                else {_alarmMinute = value;}
             }
-            else {_hour = value;}
+
         }
 
-    }
-
-    public int Minute
-    {
-        get { return _minute; }
-        set
+        public int Hour
         {
-                if (value < 0 || value > 59)
+            get { return _hour; }
+            set
             {
-                throw new ArgumentException();
+                    if (value < 0 || value > 23)
+                {
+                    throw new ArgumentException();
+                }
+                else {_hour = value;}
             }
-            else {_minute = value;}
+
         }
 
-    }
+        public int Minute
+        {
+            get { return _minute; }
+            set
+            {
+                    if (value < 0 || value > 59)
+                {
+                    throw new ArgumentException();
+                }
+                else {_minute = value;}
+            }
 
+        }
 
-    // Konstruktorn kommer här.
+        // Konstruktorer. Ser till att AlarmClock-objekt blir korrekt initierat.
+        public AlarmClock() :this(0, 0)
+        {
+        
+        }
 
+        public AlarmClock (int hour, int minute) :this(Hour, Minute, 0, 0)
+        {
 
-    // Metoden TickTock kommer här.
+        }
 
+        public AlarmClock(int hour, int minute , int alarmHour, int alarmMinute)
+        {
+            Hour = hour;
+            Minute = minute;
+            AlarmHour = alarmHour;
+            AlarmMinute = alarmMinute;
+        }
 
-    // Metoden ToString kommer här.
+        // Metoden TickTock. Anropas för att få klockan att gå en minut.
+        public bool TickTock()
+        {
+            if (Minute < 59)
+            {
+
+                Minute = 0;
+                Hour++;
+            
+            if (Hour > 23)
+            {
+
+                Hour = 0;
+            }
+
+        }
+        // Kontrollerar om den nya tiden överensstämmer med alarmtiden.
+            if (Hour == AlarmHour && Minute == AlarmMinute)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+
+        // Metoden ToString kommer här.
